cb13170f54d31930db6c1c4b6b962092a9518143
vincent.chouine <vincent.chouinard5@gmail.com>
2020-03-22 12:32:27 -0400

    Create .DS_Store

---
+++ No1/No1_a.py
@@ -0,0 +1,28 @@
+from math import sin
+from numpy import arange
+from pylab import plot,xlabel,ylabel,show
+
+def RK4 (fonct_u, fonct_v, val_init, range):
+    t0, t1 = range[0], range[1]
+    N = 10000
+    h = (t1-t0)/N
+    tpoints = np.arange(t0, t1, h)
+    upoints, vpoints = [], []
+    u, v = val_init[0], val_init[1]
+    for t in tpoints:
+        upoints.append(u)
+        vpoints.append(v)
+        k1u = h*fonct_u(u, v, t)
+        k2u = h*fonct_u(u, v+0.5*k1u, t+0.5*h)
+        k3u = h*fonct_u(u, v+0.5*k2u, t+0.5*h)
+        k4u = h*fonct_u(u, v+k3u, t+h)
+
+        k1v = h*fonct_v(u, v, t)
+        k2v = h*fonct_v(u+0.5*k1v, v, t+0.5*h)
+        k3v = h*fonct_v(x+0.5*k2v, v, t+0.5*h)
+        k4v = h*fonct_v(x +k3v, v, t+h)
+
+        u += (k1u +2*k2u +2*k3u +k4u)/6
+        v += (k1v +2*k2v +2*k3v +k4v)/6
+
+    return upoints, vpoints, tpoints
