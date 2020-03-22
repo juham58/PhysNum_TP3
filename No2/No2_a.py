0db8d7ff91bfee486d8a759fec571af198de56f1
juham58 <59977027+juham58@users.noreply.github.com>
2020-03-22 15:14:28 -0400

    Prise en compte de la vitesse

--- No2/No2_a.py
+++ No2/No2_a.py
@@ -17,6 +17,10 @@ r_Ai = np.array([1.0, 3.0])
 r_Bi = np.array([-2.0, -1.0])
 r_Ci = np.array([1.0, -1.0])
 
+v_Ai = np.array([0.0, 0.0])
+v_Bi = np.array([0.0, 0.0])
+v_Ci = np.array([0.0, 0.0])
+
 
 def F(corps, r_A, r_B, r_C):
     if corps == "A":
@@ -40,25 +44,33 @@ def mouton_3_corps(t_i, t_f, N):
     h = (t_f-t_i)/N
 
     # calcul du point r(t+h/2) avec Runge-Kutta d'ordre 4
-    k1_A = 0.5*h*F("A", r_Ai, r_Bi, r_Ci)
-    k1_B = 0.5*h*F("B", r_Ai, r_Bi, r_Ci)
-    k1_C = 0.5*h*F("C", r_Ai, r_Bi, r_Ci)
+    k1_A_v = 0.5*h*F("A", r_Ai, r_Bi, r_Ci)
+    k1_B_v = 0.5*h*F("B", r_Ai, r_Bi, r_Ci)
+    k1_C_v = 0.5*h*F("C", r_Ai, r_Bi, r_Ci)
 
-    k2_A = 0.5*h*F("A", r_Ai + 0.5*k1_A, r_Bi + 0.5*k1_B, r_Ci + 0.5*k1_C)
-    k2_B = 0.5*h*F("B", r_Ai + 0.5*k1_A, r_Bi + 0.5*k1_B, r_Ci + 0.5*k1_C)
-    k2_C = 0.5*h*F("C", r_Ai + 0.5*k1_A, r_Bi + 0.5*k1_B, r_Ci + 0.5*k1_C)
+    k2_A_v = 0.5*h*F("A", r_Ai + 0.5*k1_A_v, r_Bi + 0.5*k1_B_v, r_Ci + 0.5*k1_C_v)
+    k2_B_v = 0.5*h*F("B", r_Ai + 0.5*k1_A_v, r_Bi + 0.5*k1_B_v, r_Ci + 0.5*k1_C_v)
+    k2_C_v = 0.5*h*F("C", r_Ai + 0.5*k1_A_v, r_Bi + 0.5*k1_B_v, r_Ci + 0.5*k1_C_v)
 
-    k3_A = 0.5*h*F("A", r_Ai + 0.5*k2_A, r_Bi + 0.5*k2_B, r_Ci + 0.5*k2_C)
-    k3_B = 0.5*h*F("B", r_Ai + 0.5*k2_A, r_Bi + 0.5*k2_B, r_Ci + 0.5*k2_C)
-    k3_C = 0.5*h*F("C", r_Ai + 0.5*k2_A, r_Bi + 0.5*k2_B, r_Ci + 0.5*k2_C)
+    k3_A_v = 0.5*h*F("A", r_Ai + 0.5*k2_A_v, r_Bi + 0.5*k2_B_v, r_Ci + 0.5*k2_C_v)
+    k3_B_v = 0.5*h*F("B", r_Ai + 0.5*k2_A_v, r_Bi + 0.5*k2_B_v, r_Ci + 0.5*k2_C_v)
+    k3_C_v = 0.5*h*F("C", r_Ai + 0.5*k2_A_v, r_Bi + 0.5*k2_B_v, r_Ci + 0.5*k2_C_v)
 
-    k4_A = 0.5*h*F("A", r_Ai + 0.5*k3_A, r_Bi + 0.5*k3_B, r_Ci + 0.5*k3_C)
-    k4_B = 0.5*h*F("B", r_Ai + 0.5*k3_A, r_Bi + 0.5*k3_B, r_Ci + 0.5*k3_C)
-    k4_C = 0.5*h*F("C", r_Ai + 0.5*k3_A, r_Bi + 0.5*k3_B, r_Ci + 0.5*k3_C)
+    k4_A_v = 0.5*h*F("A", r_Ai + 0.5*k3_A_v, r_Bi + 0.5*k3_B_v, r_Ci + 0.5*k3_C_v)
+    k4_B_v = 0.5*h*F("B", r_Ai + 0.5*k3_A_v, r_Bi + 0.5*k3_B_v, r_Ci + 0.5*k3_C_v)
+    k4_C_v = 0.5*h*F("C", r_Ai + 0.5*k3_A_v, r_Bi + 0.5*k3_B_v, r_Ci + 0.5*k3_C_v)
 
-    r_A_demie = r_Ai + 1/6*(k1_A+2*k2_A+2*k3_A+k4_A)
-    r_B_demie = r_Bi + 1/6*(k1_B+2*k2_B+2*k3_B+k4_B)
-    r_C_demie = r_Ci + 1/6*(k1_C+2*k2_C+2*k3_C+k4_C)
+    v_A_demie = v_Ai + 1/6*(k1_A_v+2*k2_A_v+2*k3_A_v+k4_A_v)
+    v_B_demie = v_Bi + 1/6*(k1_B_v+2*k2_B_v+2*k3_B_v+k4_B_v)
+    v_C_demie = v_Ci + 1/6*(k1_C_v+2*k2_C_v+2*k3_C_v+k4_C_v)
+
+    r_A_demie = r_Ai + 0.5*h*v_A_demie
+    r_B_demie = r_Bi + 0.5*h*v_B_demie
+    r_C_demie = r_Ci + 0.5*h*v_C_demie
+
+    v_A = v_Ai
+    v_B = v_Bi
+    v_C = v_Ci
 
     r_A = r_Ai
     r_B = r_Bi
@@ -67,34 +79,35 @@ def mouton_3_corps(t_i, t_f, N):
     rA_arr[0][0], rA_arr[0][1] = r_A[0], r_A[1]
     rB_arr[0][0], rB_arr[0][1] = r_B[0], r_B[1]
     rC_arr[0][0], rC_arr[0][1] = r_C[0], r_C[1]
-    compteur_t = 0.0
-    compteur_h = 1
-    compteur_demies = 1
     # début des calculs par sauts
     for i, t in enumerate(t_points[1:]):
 
-        compteur_t += h
+
         #print(100*compteur_t/(t_f-t_i), "\n")
 
-        r_A = r_A + h*F("A", r_A_demie, r_B_demie, r_C_demie)
-        r_B = r_B + h*F("B", r_A_demie, r_B_demie, r_C_demie)
-        r_C = r_C + h*F("C", r_A_demie, r_B_demie, r_C_demie)
-        compteur_h += 1
+        v_A = v_A + h*F("A", r_A_demie, r_B_demie, r_C_demie)
+        v_B = v_B + h*F("B", r_A_demie, r_B_demie, r_C_demie)
+        v_C = v_C + h*F("C", r_A_demie, r_B_demie, r_C_demie)
+
+        r_A = r_A + h*v_A_demie
+        r_B = r_B + h*v_B_demie
+        r_C = r_C + h*v_C_demie
 
         # premier calcul pour un demi-saut
-        r_A_demie = r_A_demie + h*F("A", r_A, r_B, r_C)
-        r_B_demie = r_B_demie + h*F("B", r_A, r_B, r_C)
-        r_C_demie = r_C_demie + h*F("C", r_A, r_B, r_C)
-        compteur_demies += 2
+        v_A_demie = v_A_demie + h*F("A", r_A, r_B, r_C)
+        v_B_demie = v_B_demie + h*F("B", r_A, r_B, r_C)
+        v_C_demie = v_C_demie + h*F("C", r_A, r_B, r_C)
 
-        print(compteur_h)
-        print(compteur_demies/2, "\n")
+        r_A_demie = r_A_demie + h*v_A
+        r_B_demie = r_B_demie + h*v_B
+        r_C_demie = r_C_demie + h*v_C
+
         # deuxième calcul pour compléter le saut
         rA_arr[i+1][0], rA_arr[i+1][1] = r_A[0], r_A[1]
         rB_arr[i+1][0], rB_arr[i+1][1] = r_B[0], r_B[1]
         rC_arr[i+1][0], rC_arr[i+1][1] = r_C[0], r_C[1]
 
-        #print((m_A*r_A+m_B*r_B+m_C*r_C)/(m_A + m_B + m_C))  # centre de masse
+        print((m_A*r_A+m_B*r_B+m_C*r_C)/(m_A + m_B + m_C))  # centre de masse
     return {"A": rA_arr, "B": rB_arr, "C": rC_arr, "t": t_points}
 
 
