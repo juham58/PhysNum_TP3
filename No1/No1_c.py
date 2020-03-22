5948d54b8c796814f94c4f355e08574dab26c23b
vincent.chouine <vincent.chouinard5@gmail.com>
2020-03-22 18:07:38 -0400

    Update No1_c.py

--- No1/No1_c.py
+++ No1/No1_c.py
@@ -3,10 +3,22 @@ from No1_a import RK4, graph, u_pt
 def v_pt(u, v, t):
     return -(u**3.0)
 
-RK4_c1 = RK4(u_pt, v_pt, [1.0, 0.0], [0.0, 50.0])
-RK4_c10 = RK4(u_pt, v_pt, [10.0, 0.0], [0.0, 50.0])
-RK4_c50 = RK4(u_pt, v_pt, [50.0, 0.0], [0.0, 50.0])
+def periode(fonction, val_init):
+    liste1 = fonction[0]
+    liste2 = fonction[2]
+    for i in range(len(liste1)):
+        if liste1[i] > val_init:
+            return liste2[i]
+
+
+RK4_c1 = RK4(u_pt, v_pt, [1.0, 0.0], [0.0, 50.0], 10000)
+RK4_c3 = RK4(u_pt, v_pt, [3.0, 0.0], [0.0, 50.0], 100000)
+RK4_c5 = RK4(u_pt, v_pt, [5.0, 0.0], [0.0, 50.0], 100000)
+
+print("La periode pour x(0) = 1 est de " + str(periode(RK4_c1, 1.0)) + " secondes.")
+print("La periode pour x(0) = 3 est de " + str(periode(RK4_c3, 3.0)) + " secondes.")
+print("La periode pour x(0) = 5 est de " + str(periode(RK4_c5, 5.0)) + " secondes.")
 
 graph(RK4_c1[0], RK4_c1[2],"Graphique de la position en fonction du temps d'un oscillateur anharmonique en 1D avec x(0)=1")
-graph(RK4_c1[0], RK4_c1[2],"Graphique de la position en fonction du temps d'un oscillateur anharmonique en 1D avec x(0)=10")
-graph(RK4_c1[0], RK4_c1[2],"Graphique de la position en fonction du temps d'un oscillateur anharmonique en 1D avec x(0)=50")
+graph(RK4_c3[0], RK4_c3[2],"Graphique de la position en fonction du temps d'un oscillateur anharmonique en 1D avec x(0)=3")
+graph(RK4_c5[0], RK4_c5[2],"Graphique de la position en fonction du temps d'un oscillateur anharmonique en 1D avec x(0)=5")
