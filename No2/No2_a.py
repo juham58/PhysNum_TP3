3c09360e48b18251555f35591154a045bf1e58b8
juham58 <59977027+juham58@users.noreply.github.com>
2020-03-21 20:08:02 -0400

    Changement pour saute-mouton au lieu de RK4. Ça marche mieux

--- No2/No2_a.py
+++ No2/No2_a.py
@@ -27,52 +27,68 @@ def F(corps, r_A, r_B, r_C):
         return -4*constants.pi**2*(m_A*((r_C-r_A)/(np.linalg.norm(r_C-r_A)**3))+m_B*((r_C-r_B)/(np.linalg.norm(r_C-r_B)**3)))
 
 
-def RK4_3_corps(t_i, t_f, N):
-    r_A, r_B, r_C = r_Ai, r_Bi, r_Ci
+def mouton_3_corps(t_i, t_f, N):
+    r_A_1, r_B_1, r_C_1 = r_Ai, r_Bi, r_Ci
     t_points = np.linspace(t_i, t_f, N)
     rA_arr = np.zeros((len(t_points), 2))
     rB_arr = np.zeros((len(t_points), 2))
     rC_arr = np.zeros((len(t_points), 2))
     h = (t_f-t_i)/N
+
+    k1_A, k1_B, k1_C = h*F("A", r_Ai, r_Bi, r_Ci), h*F("B", r_Ai, r_Bi, r_Ci), h*F("C", r_Ai, r_Bi, r_Ci)
+    k2_A, k2_B, k2_C = h*F("A", r_Ai + 0.5*k1_A, r_Bi + 0.5*k1_B, r_Ci + 0.5*k1_C), h*F("B", r_Ai + 0.5*k1_A, r_Bi + 0.5*k1_B, r_Ci + 0.5*k1_C), h*F("C", r_Ai + 0.5*k1_A, r_Bi + 0.5*k1_B, r_Ci + 0.5*k1_C)
+    r_A_2, r_B_2, r_C_2 = r_Ai + k2_A, r_Bi + k2_B, r_Ci + k2_C
+
+    r_A, r_B, r_C = r_A_1 + h*F("A", r_A_2, r_B_2, r_C_2), r_B_1 + h*F("B", r_A_2, r_B_2, r_C_2), r_C_1 + h*F("C", r_A_2, r_B_2, r_C_2)
     for i, t in enumerate(t_points):
         rA_arr[i][0], rA_arr[i][1] = r_A[0], r_A[1]
         rB_arr[i][0], rB_arr[i][1] = r_B[0], r_B[1]
         rC_arr[i][0], rC_arr[i][1] = r_C[0], r_C[1]
 
-        k1_A, k1_B, k1_C = h*F("A", r_A, r_B, r_C), h*F("B", r_A, r_B, r_C), h*F("C", r_A, r_B, r_C)
-        k2_A, k2_B, k2_C = h*F("A", r_A + 0.5*k1_A, r_B + 0.5*k1_B, r_C + 0.5*k1_C), h*F("B", r_A + 0.5*k1_A, r_B + 0.5*k1_B, r_C + 0.5*k1_C), h*F("C", r_A + 0.5*k1_A, r_B + 0.5*k1_B, r_C + 0.5*k1_C)
-        k3_A, k3_B, k3_C = h*F("A", r_A + 0.5*k2_A, r_B + 0.5*k2_B, r_C + 0.5*k2_C), h*F("B", r_A + 0.5*k2_A, r_B + 0.5*k2_B, r_C + 0.5*k2_C), h*F("C", r_A + 0.5*k2_A, r_B + 0.5*k2_B, r_C + 0.5*k2_C)
-        k4_A, k4_B, k4_C = h*F("A", r_A+k3_A, r_B+k3_B, r_C+k3_C), h*F("B", r_A+k3_A, r_B+k3_B, r_C+k3_C), h*F("C", r_A+k3_A, r_B+k3_B, r_C+k3_C)
+        # premier calcul pour un demi-saut
+        r_A_1, r_B_1, r_C_1 = r_A_2, r_B_2, r_C_2
+        r_A_2, r_B_2, r_C_2 = r_A, r_B, r_C
+        r_A, r_B, r_C = r_A_1 + h*F("A", r_A_2, r_B_2, r_C_2), r_B_1 + h*F("B", r_A_2, r_B_2, r_C_2), r_C_1 + h*F("C", r_A_2, r_B_2, r_C_2)
 
-        r_A += (k1_A + 2*k2_A + 2*k3_A + k4_A)/6
-        r_B += (k1_B + 2*k2_B + 2*k3_B + k4_B)/6
-        r_C += (k1_C + 2*k2_C + 2*k3_C + k4_C)/6
+        # deuxième calcul pour compléter le saut
+        r_A_1, r_B_1, r_C_1 = r_A_2, r_B_2, r_C_2
+        r_A_2, r_B_2, r_C_2 = r_A, r_B, r_C
+        r_A, r_B, r_C = r_A_1 + h*F("A", r_A_2, r_B_2, r_C_2), r_B_1 + h*F("B", r_A_2, r_B_2, r_C_2), r_C_1 + h*F("C", r_A_2, r_B_2, r_C_2)
+
     return {"A": rA_arr, "B": rB_arr, "C": rC_arr, "t": t_points}
 
 
-#data_A = [r_Ai]
-#data_B = [r_Bi]
-#data_C = [r_Ci]
-
-
-
 def graph_3_corps(t_i, t_f, N):
-    RK4 = RK4_3_corps(t_i, t_f, N)
+    data_ligne_A = []
+    data_ligne_B = []
+    data_ligne_C = [r_Ci]
+
+    mouton = mouton_3_corps(t_i, t_f, N)
     fig, ax = plt.subplots()
-    ax.set(xlim=(-20, 20), ylim=(-20, 20))
+    ax.set(xlim=(-5, 5), ylim=(-5, 5))
 
-    point_A, = ax.plot(r_Ai, 'bo-')
-    point_B, = ax.plot(r_Bi, 'go-')
-    point_C, = ax.plot(r_Ci, 'ro-')
+    point_A, = ax.plot(r_Ai, 'b.')
+    point_B, = ax.plot(r_Bi, 'g.')
+    point_C, = ax.plot(r_Ci, 'r.')
+    #ligne_A, = ax.plot(r_Ai, 'b-')
+    #ligne_B, = ax.plot(r_Bi, 'g-')
+    #ligne_C, = ax.plot(r_Ci, 'r-')
 
-    animation_A = lambda i: point_A.set_data(RK4["A"][i])
-    animation_B = lambda i: point_B.set_data(RK4["B"][i])
-    animation_C = lambda i: point_C.set_data(RK4["C"][i])
+    anim_point_A = lambda i: point_A.set_data(mouton["A"][i])
+    anim_point_B = lambda i: point_B.set_data(mouton["B"][i])
+    anim_point_C = lambda i: point_C.set_data(mouton["C"][i])
+    #anim_ligne_A = lambda i: ligne_A.set_data(data_ligne_A.append(mouton["A"][i]))
+    #anim_ligne_B = lambda i: ligne_B.set_data(data_ligne_B.append(mouton["B"][i]))
+    #anim_ligne_C = lambda i: ligne_C.set_data(data_ligne_C.append(mouton["C"][i]))
 
-    frames_anim = len(RK4["t"])
-    graph_anim_A = FuncAnimation(fig, animation_A, frames=frames_anim, interval=1)
-    graph_anim_B = FuncAnimation(fig, animation_B, frames=frames_anim, interval=1)
-    graph_anim_C = FuncAnimation(fig, animation_C, frames=frames_anim, interval=1)
+    frames_anim = len(mouton["t"])
+    graph_anim_A = FuncAnimation(fig, anim_point_A, frames=frames_anim, interval=1)
+    graph_anim_B = FuncAnimation(fig, anim_point_B, frames=frames_anim, interval=1)
+    graph_anim_C = FuncAnimation(fig, anim_point_C, frames=frames_anim, interval=1)
+    #graph_anim_A = FuncAnimation(fig, anim_ligne_A, frames=frames_anim, interval=1)
+    #graph_anim_B = FuncAnimation(fig, anim_ligne_B, frames=frames_anim, interval=1)
+    #graph_anim_C = FuncAnimation(fig, anim_ligne_C, frames=frames_anim, interval=1)
     plt.show()
 
-graph_3_corps(0, 1, 50000)
+
+graph_3_corps(0, 10, 80000)
