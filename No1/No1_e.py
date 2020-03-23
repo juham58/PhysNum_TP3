657841af83560257d7de43141e12873f06172a54
vincentpiano123 <59986183+vincentpiano123@users.noreply.github.com>
2020-03-22 23:01:51 -0400

    Update No1_e.py

--- No1/No1_e.py
+++ No1/No1_e.py
@@ -1,20 +1,30 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
 import scipy.constants as cst
-from No1_d import graph
 
-#vdp avec epsilon=1 déjà dans l'équation de x3
+# vdp avec epsilon=1 déjà dans l'équation de x3
 def vdp(t,x):
     x1 = x[0]
     x2 = x[1]
     x3 = -x1 - (x1 ** 2 - 1) * x2
     return np.array([x2, x3], float)
 
-#fonction qui fait la liste de points d'intervalle h entre t=0 et t=8*pi
+# fonction qui fait la liste de points d'intervalle h entre t=0 et t=8*pi
 def rangetemps(N, trange):
    return np.arange(trange[0], trange[1], (trange[1] - trange[0]) / N)
 
-#on pose le range, on trouve notre liste de temps et on solutionne pour obtenir notre graphique
+def graph(axe_y1, axe_y2, axe_x, titre):
+    plt.figure(figsize=(16,8))
+    plt.xlabel("x(t)")
+    plt.ylabel("v(t)")
+    plt.title(titre)
+    plt.grid()
+    plt.plot(axe_x, axe_y1, "k-")
+    plt.plot(axe_x, axe_y2, "r-")
+    plt.show()
+
+# on pose le range, on trouve notre liste de temps et on solutionne pour obtenir notre graphique
 if __name__ == "__main__":
     trange = [0, 8 * cst.pi]
     t = rangetemps(1000000, trange)
@@ -22,4 +32,4 @@ if __name__ == "__main__":
     solution = solve_ivp(vdp, [0, 8 * cst.pi], [0.5, 0], t_eval=t, method='RK45')
     sol = solution.y
 
-    graph(sol[0], sol[1], "Graphique de l'espace des phases du système de van der Pol avec x(0) = 0.5 et v(0) = 0.0")
+    graph(sol[0], sol[1], t, "Graphique de l'espace des phases du système de van der Pol avec x(0) = 0.5 et v(0) = 0.0")
