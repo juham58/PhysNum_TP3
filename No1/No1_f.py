5e888b5cc2f60eb5752f61b3ae18cb379306cafe
vincent.chouine <vincent.chouinard5@gmail.com>
2020-03-22 22:09:00 -0400

    Update No1_f.py

--- No1/No1_f.py
+++ No1/No1_f.py
@@ -16,7 +16,13 @@ if __name__ == "__main__":
     solution3 = solve_ivp(vdp, [0, 8 * cst.pi], [3.0, 0], t_eval=t, method='RK45')
     sol3 = solution3.y
 
+#Par curiosité, nous avons même testé avec une vitesse initiale v(0) = 3:
+    solution4 = solve_ivp(vdp, [0, 8 * cst.pi], [3.0, 3.0], t_eval=t, method='RK45')
+    sol4 = solution4.y
 
+#graphique avec x(0) = 1, 2, 3
     graph(sol1[0], sol1[1], "Graphique de l'espace des phases du système de van der Pol avec x(0) = 1.0 et v(0) = 0.0")
     graph(sol2[0], sol2[1], "Graphique de l'espace des phases du système de van der Pol avec x(0) = 2.0 et v(0) = 0.0")
     graph(sol3[0], sol3[1], "Graphique de l'espace des phases du système de van der Pol avec x(0) = 3.0 et v(0) = 0.0")
+#graphique d'une vitesse initiale non-nulle
+    graph(sol4[0], sol4[1], "Graphique de l'espace des phases du système de van der Pol avec x(0) = 3.0 et v(0) = 3.0")
